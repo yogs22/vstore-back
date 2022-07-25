321c5603e776595ee6892bd16088df5dd73a382f
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2022-07-26 06:34:46 +0700

    [Category] Add create action

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,7 +1,29 @@
-module.exports={
+const Category = require('./model')
+
+module.exports = {
   index: async(req, res) => {
     try {
       res.render('admin/category/view_category')
+    } catch (e) {
+      console.log(e)
+    }
+  },
+  viewCreate: async(req, res) => {
+    try {
+      res.render('admin/category/create')
+    } catch (e) {
+      console.log(e)
+    }
+  },
+
+  actionCreate: async(req, res) => {
+    try {
+      const { name } = req.body
+
+      let category = await Category({ name })
+      await category.save();
+
+      res.redirect('/category')
     } catch (e) {
       console.log(e)
     }
