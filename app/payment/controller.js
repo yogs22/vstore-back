147873f199c8b46b7872bbb0918039b09663e36f
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2022-07-30 18:08:38 +0700

    Add Payment CRUD

---
+++ app/payment/controller.js
@@ -0,0 +1,101 @@
+const Payment = require('./model')
+const Bank = require('../bank/model')
+
+module.exports = {
+  index: async(req, res) => {
+    try {
+      const alertMessage = req.flash("alertMessage")
+      const alertStatus = req.flash("alertStatus")
+
+      const alert = { message: alertMessage, status: alertStatus }
+      const payment = await Payment.find().populate('banks')
+      res.render('admin/payment/view_payment', {
+        payment, alert
+      })
+    } catch (e) {
+      req.flash('alertMessage', `${e.message}`)
+      req.flash('alertStatus', `danger`)
+      res.redirect('payment')
+    }
+  },
+  viewCreate: async(req, res) => {
+    try {
+      const banks = await Bank.find()
+      res.render('admin/payment/create', {
+        banks
+      })
+    } catch (e) {
+      req.flash('alertMessage', `${e.message}`)
+      req.flash('alertStatus', `danger`)
+      res.redirect('payment')
+    }
+  },
+  actionCreate: async(req, res) => {
+    try {
+      const { type, banks } = req.body
+
+      let payment = await Payment({ type, banks })
+      await payment.save();
+
+      req.flash('alertMessage', 'Berhasil tambah pembayaran')
+      req.flash('alertStatus', 'success')
+
+      res.redirect('/payment')
+    } catch (e) {
+      req.flash('alertMessage', `${e.message}`)
+      req.flash('alertStatus', `danger`)
+      res.redirect('payment')
+    }
+  },
+  viewEdit: async(req, res) => {
+    try {
+      const { id } = req.params
+      const payment = await Payment.findOne({_id: id})
+      const banks = await Bank.find()
+
+      res.render('admin/payment/edit', {
+        payment, banks
+      })
+    } catch (e) {
+      req.flash('alertMessage', `${e.message}`)
+      req.flash('alertStatus', `danger`)
+      res.redirect('payment')
+    }
+  },
+  actionEdit: async(req, res) => {
+    try {
+      const { id } = req.params
+      const { type, banks } = req.body
+
+      const payment = await Payment.findOneAndUpdate({
+        _id: id
+      }, { type, banks })
+
+      req.flash('alertMessage', 'Berhasil edit pembayaran')
+      req.flash('alertStatus', 'success')
+
+      res.redirect('/payment')
+    } catch (e) {
+      req.flash('alertMessage', `${e.message}`)
+      req.flash('alertStatus', `danger`)
+      res.redirect('payment')
+    }
+  },
+  actionDelete: async(req, res) => {
+    try {
+      const { id } = req.params;
+      const payment = await Payment.findOneAndRemove({
+        _id: id
+      })
+
+      req.flash('alertMessage', 'Berhasil hapus pembayaran')
+      req.flash('alertStatus', 'success')
+
+      res.redirect('/payment')
+    } catch (e) {
+      req.flash('alertMessage', `${e.message}`)
+      req.flash('alertStatus', `danger`)
+      res.redirect('payment')
+    }
+  }
+}
